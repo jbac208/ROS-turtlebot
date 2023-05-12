e853318fa15c55706ce59ca518e5063d54344ceb
jbac208 <jbac208@aucklanduni.ac.nz>
2023-05-12 15:23:48 +1200

    movement node need to test

---
+++ movement.py
@@ -0,0 +1,39 @@
+import rospy
+from geometry_msgs.msg import Twist
+
+class MovementNode():
+    def __init__(self):
+        # Initialise
+        rospy.init_node('MovementNode', anonymous=False)
+
+        # Tell user how to stop TurtleBot
+        rospy.loginfo("To stop TurtleBot CTRL + C")
+
+        # What function to call when you ctrl + c
+        rospy.on_shutdown(self.shutdown)
+
+        # Create a publisher which can "talk" to TurtleBot and tell it to move
+        self.cmd_vel=rospy.Publisher(
+            '/cmd_vel_mux/input/teleop',
+            Twist,
+            queue_size=10)
+        
+        # Move at freq 10Hz
+        r = rospy.Rate(10)
+
+        # Twist is a datatype for velocity
+        move_cmd = Twist()
+
+        # Forward at 0.2m/s
+        move_cmd.linear.x = 0.2
+
+        # Continue until ctrl + c
+        while not rospy.is_shutdown():
+            # publish the velocity
+            self.cmd_vel.publish(move_cmd)
+            # wait for 0.1 seconds (10 Hz) and publish again
+            r.sleep()
+
+    def shutdown(self):
+        # Stop turtlebot
+        rospy.loginfo("Stop TurtleBot")
